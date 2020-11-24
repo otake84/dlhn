48c3904f44587e7148902ae36456125142b8178c
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 04:16:08 +0900

    Add the serialize function to Body

--- src/body.rs
+++ src/body.rs
@@ -1,4 +1,20 @@
+use crate::header::Header;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Body {
     Boolean(bool),
+}
+
+impl Body {
+    pub(crate) fn serialize(&self, header: &Header) -> Result<Vec<u8>, ()> {
+        match header {
+            Header::Boolean => {
+                match self {
+                    Body::Boolean(_) => {
+                        Ok(vec![1])
+                    }
+                }
+            }
+        }
+    }
 }
--- src/serializer.rs
+++ src/serializer.rs
@@ -1,13 +1,5 @@
 use crate::{body::Body, header::Header};
 
 pub fn serialize(header: &Header, body: &Body) -> Result<Vec<u8>, ()> {
-    match header {
-        Header::Boolean => {
-            match body {
-                Body::Boolean(_) => {
-                    Ok(vec![1])
-                }
-            }
-        }
-    }
+    body.serialize(header)
 }
