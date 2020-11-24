4d7d4766a8a549b5d66e6203143576f9f1f26050
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 03:51:09 +0900

    impl Default for Header

--- src/header.rs
+++ src/header.rs
@@ -1,9 +1,15 @@
 #[derive(Clone, Debug, PartialEq)]
 pub struct Header(Vec<Type>);
 
-impl Header {
-    pub fn new() -> Self {
+impl Default for Header {
+    fn default() -> Self {
         Self(Vec::new())
+    }
+}
+
+impl Header {
+    pub fn new(types: Vec<Type>) -> Self {
+        Self(types)
     }
 
     pub fn types(&self) -> &[Type] {
