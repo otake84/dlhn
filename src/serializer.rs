5faf26bcc070a5550c6c14eecf61aece302829a4
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 04:10:23 +0900

    Added an implementation for Bool to the serialize function

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,5 +1,13 @@
 use crate::{body::Body, header::Header};
 
-pub fn serialize(header: Header, body: Body) -> Result<Vec<u8>, ()> {
-    Err(())
+pub fn serialize(header: &Header, body: &Body) -> Result<Vec<u8>, ()> {
+    match header {
+        Header::Bool => {
+            match body {
+                Body::Bool(_) => {
+                    Ok(vec![1])
+                }
+            }
+        }
+    }
 }
