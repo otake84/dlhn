6b6a14e948d776431116be9fe26cc193788c36c8
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 03:46:05 +0900

    Add the new function to Header

--- src/header.rs
+++ src/header.rs
@@ -1,6 +1,12 @@
 #[derive(Clone, Debug, PartialEq)]
 pub struct Header(Vec<Type>);
 
+impl Header {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Type {
     Bool,
