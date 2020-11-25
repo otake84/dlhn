15e9f7ea5a19a739d86590842e301b3f8df59181
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 17:05:02 +0900

    Add the serialize function to Header

--- src/header.rs
+++ src/header.rs
@@ -2,3 +2,13 @@
 pub enum Header {
     Boolean,
 }
+
+impl Header {
+    pub(crate) fn serialize(&self) -> Vec<u8> {
+        match self {
+            Header::Boolean => {
+                vec![0]
+            }
+        }
+    }
+}
