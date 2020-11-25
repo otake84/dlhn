a7d8bcaf4231d8b852bb60c3be29da28ed0bb998
Shogo Otake <shogo.otake@gmail.com>
2020-11-25 17:07:04 +0900

    Modified to include a header in the serialized results

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,5 +1,12 @@
 use crate::{body::Body, header::Header};
 
 pub fn serialize(header: &Header, body: &Body) -> Result<Vec<u8>, ()> {
-    body.serialize(header)
+    let mut serialized_header= header.serialize();
+    match body.serialize(header) {
+        Ok(mut value) => {
+            serialized_header.append(&mut value);
+            Ok(serialized_header)
+        }
+        Err(_) => Err(())
+    }
 }
