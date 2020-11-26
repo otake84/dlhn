6011d70103f466d540ca9b344196dd5d3d5a57f5
Shogo Otake <shogo.otake@gmail.com>
2020-11-26 19:38:08 +0900

    Add the deserialize function to Header

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,5 @@
+use std::io::{BufReader, Read};
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Header {
     Boolean,
@@ -14,5 +16,33 @@ impl Header {
                 vec![1]
             }
         }
+    }
+
+    pub(crate) fn deserialize<R: Read>(read: R) -> Result<Header, ()> {
+        let mut reader = BufReader::new(read);
+        let mut buf = [0u8; 1];
+        reader.read_exact(&mut buf).or(Err(()))?;
+
+        if let Some(first) = buf.first() {
+            match first {
+                0 => Ok(Header::Boolean),
+                1 => Ok(Header::UInt8),
+                _ => Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Header;
+
+    #[test]
+    fn deserialize() {
+        assert_eq!(Header::deserialize(&[0u8] as &[u8]), Ok(Header::Boolean));
+        assert_eq!(Header::deserialize(&[1u8] as &[u8]), Ok(Header::UInt8));
     }
 }
