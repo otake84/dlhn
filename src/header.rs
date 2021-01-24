2eb414bac5b37053a58f56f02b51f82a5a0225ec
Shogo Otake <shogo.otake@gmail.com>
2021-01-25 02:46:59 +0900

    Refactoring

--- src/header.rs
+++ src/header.rs
@@ -248,85 +248,90 @@ mod tests {
     fn deserialize() {
         assert_eq!(
             Header::deserialize(&mut BufReader::new(
-                [Header::OPTIONAL_CODE, Header::Boolean.code()].as_ref()
+                Header::Optional(Box::new(Header::Boolean))
+                    .serialize()
+                    .as_slice()
             )),
             Ok(Header::Optional(Box::new(Header::Boolean)))
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Boolean.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Boolean.serialize().as_slice())),
             Ok(Header::Boolean)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::UInt8.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::UInt8.serialize().as_slice())),
             Ok(Header::UInt8)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::UInt16.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::UInt16.serialize().as_slice())),
             Ok(Header::UInt16)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::UInt32.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::UInt32.serialize().as_slice())),
             Ok(Header::UInt32)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::UInt64.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::UInt64.serialize().as_slice())),
             Ok(Header::UInt64)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Int8.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Int8.serialize().as_slice())),
             Ok(Header::Int8)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Int16.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Int16.serialize().as_slice())),
             Ok(Header::Int16)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Int32.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Int32.serialize().as_slice())),
             Ok(Header::Int32)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Int64.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Int64.serialize().as_slice())),
             Ok(Header::Int64)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Float32.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Float32.serialize().as_slice())),
             Ok(Header::Float32)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Float64.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Float64.serialize().as_slice())),
             Ok(Header::Float64)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::BigInt.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::BigInt.serialize().as_slice())),
             Ok(Header::BigInt)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::BigDecimal.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(
+                Header::BigDecimal.serialize().as_slice()
+            )),
             Ok(Header::BigDecimal)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::String.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::String.serialize().as_slice())),
             Ok(Header::String)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Binary.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Binary.serialize().as_slice())),
             Ok(Header::Binary)
         );
         assert_eq!(
             Header::deserialize(&mut BufReader::new(
-                [Header::ARRAY_CODE, Header::Boolean.code()].as_ref()
+                Header::Array(Box::new(Header::Boolean))
+                    .serialize()
+                    .as_slice()
             )),
             Ok(Header::Array(Box::new(Header::Boolean)))
         );
         assert_eq!(
             Header::deserialize(&mut BufReader::new(
-                [
-                    vec![Header::MAP_CODE],
-                    vec![Header::Boolean.code()],
-                    Header::serialize_map_key("test"),
-                    Header::Boolean.serialize()
-                ]
-                .concat()
+                Header::Map({
+                    let mut map = IndexMap::new();
+                    map.insert(String::from("test"), Header::Boolean);
+                    map
+                })
+                .serialize()
                 .as_slice()
             )),
             Ok(Header::Map(
@@ -344,11 +349,11 @@ mod tests {
             )))))
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::Date.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::Date.serialize().as_slice())),
             Ok(Header::Date)
         );
         assert_eq!(
-            Header::deserialize(&mut BufReader::new([Header::DateTime.code()].as_ref())),
+            Header::deserialize(&mut BufReader::new(Header::DateTime.serialize().as_slice())),
             Ok(Header::DateTime)
         );
     }
