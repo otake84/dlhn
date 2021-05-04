00b25f84baa65cdf719e3f219a0f78af86f04578
Shogo Otake <shogo.otake@gmail.com>
2021-05-05 03:55:09 +0900

    Implement deserialize_f64

--- dullahan_serde/src/de.rs
+++ dullahan_serde/src/de.rs
@@ -122,7 +122,9 @@ impl<'de , 'a, R: Read> de::Deserializer<'de> for &'a mut Deserializer<'de, R> {
     fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: de::Visitor<'de> {
-        todo!()
+        let mut body_buf: [u8; 8] = unsafe { MaybeUninit::uninit().assume_init() };
+        self.reader.read_exact(&mut body_buf).or(Err(Error::Read))?;
+        visitor.visit_f64(f64::from_le_bytes(body_buf))
     }
 
     fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -290,6 +292,16 @@ mod tests {
     #[test]
     fn deserialize_f32() {
         IntoIter::new([-f32::INFINITY, f32::MIN, 0f32, f32::MAX, f32::INFINITY]).for_each(|v| {
+            let buf = serialize(v);
+            let mut reader = buf.as_slice();
+            let mut deserializer = Deserializer::new(&mut reader);
+            assert_eq!(v, Deserialize::deserialize(&mut deserializer).unwrap());
+        });
+    }
+
+    #[test]
+    fn deserialize_f64() {
+        IntoIter::new([-f64::INFINITY, f64::MIN, 0f64, f64::MAX, f64::INFINITY]).for_each(|v| {
             let buf = serialize(v);
             let mut reader = buf.as_slice();
             let mut deserializer = Deserializer::new(&mut reader);
