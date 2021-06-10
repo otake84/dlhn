93c200c6bdf488408e3f6f322e04e9da00cfaf22
Shogo Otake <shogo.otake@gmail.com>
2021-06-10 12:41:42 +0900

    Implement deserialize for BigDecimal

--- dullahan_serde/src/format/big_decimal.rs
+++ dullahan_serde/src/format/big_decimal.rs
@@ -1,5 +1,28 @@
 use bigdecimal::{BigDecimal, Zero};
-use serde::{Serializer, ser::SerializeSeq};
+use num_bigint::BigInt;
+use serde::{Deserializer, Serializer, de::{self, SeqAccess, Unexpected, Visitor}, ser::SerializeSeq};
+use crate::de::Error;
+
+struct BigDecimalVisitor;
+
+impl<'de> Visitor<'de> for BigDecimalVisitor {
+    type Value = BigDecimal;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("format error")
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+            A: SeqAccess<'de>, {
+                let digits = BigInt::from_signed_bytes_le(seq.next_element::<Vec<u8>>()?.ok_or(de::Error::invalid_value(Unexpected::Seq, &Error::Read))?.as_slice());
+                if digits.is_zero() {
+                    Ok(BigDecimal::from(digits))
+                } else {
+                    Ok(BigDecimal::new(digits, seq.next_element::<i64>()?.ok_or(de::Error::invalid_value(Unexpected::Seq, &Error::Read))?))
+                }
+    }
+}
 
 pub fn serialize<T: Serializer>(value: &BigDecimal, serializer: T) -> Result<T::Ok, T::Error> {
     let mut seq = serializer.serialize_seq(None)?;
@@ -15,14 +38,19 @@ pub fn serialize<T: Serializer>(value: &BigDecimal, serializer: T) -> Result<T::
     seq.end()
 }
 
+pub fn deserialize<'de, T: Deserializer<'de>>(deserializer: T) -> Result<BigDecimal, T::Error> {
+    deserializer.deserialize_tuple(2, BigDecimalVisitor)
+}
+
 #[cfg(test)]
 mod tests {
+    use std::array::IntoIter;
     use bigdecimal::BigDecimal;
     use num_bigint::BigInt;
-    use serde::Serialize;
-    use crate::ser::Serializer;
+    use serde::{Deserialize, Serialize};
+    use crate::{de::Deserializer, ser::Serializer};
 
-    #[derive(Debug, PartialEq, Serialize)]
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
     struct Test {
         #[serde(with = "crate::format::big_decimal")]
         value: BigDecimal,
@@ -41,6 +69,30 @@ mod tests {
         assert_eq!(encode_big_decimal(BigDecimal::new(BigInt::from(1), -65)), [1, 1, 129, 1]);
         assert_eq!(encode_big_decimal(BigDecimal::new(BigInt::from(i16::MIN), 0)), [2, 0, 128, 0]);
         assert_eq!(encode_big_decimal(BigDecimal::new(BigInt::from(i16::MAX), 0)), [2, 255, 127, 0]);
+    }
+
+    #[test]
+    fn deserialize() {
+        fn assert_big_decimal(value: BigDecimal) {
+            let buf = encode_big_decimal(value.clone());
+            let mut reader = buf.as_slice();
+            let mut deserializer = Deserializer::new(&mut reader);
+            let result = Test::deserialize(&mut deserializer).unwrap();
+            assert_eq!(result, Test { value });
+        }
+
+        IntoIter::new([
+            BigDecimal::from(0),
+            BigDecimal::new(BigInt::from(1), 0),
+            BigDecimal::new(BigInt::from(1), -1),
+            BigDecimal::new(BigInt::from(1), 1),
+            BigDecimal::new(BigInt::from(1), 63),
+            BigDecimal::new(BigInt::from(1), 64),
+            BigDecimal::new(BigInt::from(1), -64),
+            BigDecimal::new(BigInt::from(1), -65),
+            BigDecimal::new(BigInt::from(i16::MIN), 0),
+            BigDecimal::new(BigInt::from(i16::MAX), 0),
+        ]).for_each(assert_big_decimal);
     }
 
     fn encode_big_decimal(value: BigDecimal) -> Vec<u8> {
