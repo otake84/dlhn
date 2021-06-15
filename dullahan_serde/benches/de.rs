1ab3dc44eaf0fe7ff8c7a681b7f7024e44cafb46
Shogo Otake <shogo.otake@gmail.com>
2021-06-15 19:38:41 +0900

    Refactoring

--- dullahan_serde/benches/de.rs
+++ dullahan_serde/benches/de.rs
@@ -4,100 +4,70 @@ use iai::main;
 use serde::{Deserialize, Serialize};
 
 fn deserialize_u8() -> u8 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    u8::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(u8::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     u8::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_u16() -> u16 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    u16::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(u16::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     u16::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_u32() -> u32 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    u32::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(u32::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     u32::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_u64() -> u64 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    u64::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(u64::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     u64::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_u128() -> u128 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    u128::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(u128::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     u128::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_i8() -> i8 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    i8::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(i8::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     i8::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_i16() -> i16 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    i16::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(i16::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     i16::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_i32() -> i32 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    i32::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(i32::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     i32::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_i64() -> i64 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    i64::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(i64::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     i64::deserialize(&mut deserializer).unwrap()
 }
 
 fn deserialize_i128() -> i128 {
-    let mut buf = Vec::new();
-    let mut serializer = Serializer::new(&mut buf);
-    i128::MAX.serialize(&mut serializer).unwrap();
-
+    let buf = serialize(i128::MAX);
     let mut reader = buf.as_slice();
     let mut deserializer = Deserializer::new(&mut reader);
     i128::deserialize(&mut deserializer).unwrap()
