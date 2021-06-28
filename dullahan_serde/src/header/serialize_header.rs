0a26765c0e30896ecd3e1f293a71c958b8fac392
Shogo Otake <shogo.otake@gmail.com>
2021-06-28 21:02:30 +0900

    impl SerializeHeader for tuples

--- dullahan_serde/src/header/serialize_header.rs
+++ dullahan_serde/src/header/serialize_header.rs
@@ -2,6 +2,7 @@ use std::io::{Result, Write};
 use bigdecimal::BigDecimal;
 use num_bigint::{BigInt, BigUint};
 use serde_bytes::Bytes;
+use crate::leb128::Leb128;
 
 const UNIT_CODE: u8 = 0;
 const OPTIONAL_CODE: u8 = 1;
@@ -36,129 +37,169 @@ const EXTENSION64_CODE: u8 = 29;
 const EXTENSION_CODE: u8 = 30;
 
 trait SerializeHeader {
-    fn serialize_header<W: Write>(writer: W) -> Result<()>;
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()>;
 }
 
 impl SerializeHeader for () {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[UNIT_CODE])
     }
 }
 
 impl<T: SerializeHeader> SerializeHeader for Option<T> {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[OPTIONAL_CODE])?;
         T::serialize_header(writer)
     }
 }
 
 impl SerializeHeader for bool {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[BOOLEAN_CODE])
     }
 }
 
 impl SerializeHeader for u8 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[UINT8_CODE])
     }
 }
 
 impl SerializeHeader for u16 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[UINT16_CODE])
     }
 }
 
 impl SerializeHeader for u32 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[UINT32_CODE])
     }
 }
 
 impl SerializeHeader for u64 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[UINT64_CODE])
     }
 }
 
 impl SerializeHeader for i8 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[INT8_CODE])
     }
 }
 
 impl SerializeHeader for i16 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[INT16_CODE])
     }
 }
 
 impl SerializeHeader for i32 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[INT32_CODE])
     }
 }
 
 impl SerializeHeader for i64 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[INT64_CODE])
     }
 }
 
 impl SerializeHeader for f32 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[FLOAT32_CODE])
     }
 }
 
 impl SerializeHeader for f64 {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[FLOAT64_CODE])
     }
 }
 
 impl SerializeHeader for BigUint {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[BIG_UINT_CODE])
     }
 }
 
 impl SerializeHeader for BigInt {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[BIG_INT_CODE])
     }
 }
 
 impl SerializeHeader for BigDecimal {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[BIG_DECIMAL_CODE])
     }
 }
 
 impl SerializeHeader for &str {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[STRING_CODE])
     }
 }
 
 impl SerializeHeader for String {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[STRING_CODE])
     }
 }
 
 impl SerializeHeader for Bytes {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[BINARY_CODE])
     }
 }
 
 impl<T: SerializeHeader> SerializeHeader for Vec<T> {
-    fn serialize_header<W: Write>(mut writer: W) -> Result<()> {
+    fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
         writer.write_all(&[ARRAY_CODE])?;
         T::serialize_header(writer)
     }
+}
+
+macro_rules! tuple_impls {
+    ($($len:expr => ($($name:ident)+))+) => {
+        $(
+            impl<$($name),+> SerializeHeader for ($($name,)+)
+            where
+                $($name: SerializeHeader,)+
+            {
+                fn serialize_header<W: Write>(writer: &mut W) -> Result<()> {
+                    writer.write_all(&[TUPLE_CODE])?;
+                    let (buf, size) = ($len as usize).encode_leb128();
+                    writer.write_all(&buf[..size])?;
+                    $(
+                        $name::serialize_header(writer)?;
+                    )+
+                    Ok(())
+                }
+            }
+        )+
+    }
+}
+
+tuple_impls! {
+    1 => (T0)
+    2 => (T0 T1)
+    3 => (T0 T1 T2)
+    4 => (T0 T1 T2 T3)
+    5 => (T0 T1 T2 T3 T4)
+    6 => (T0 T1 T2 T3 T4 T5)
+    7 => (T0 T1 T2 T3 T4 T5 T6)
+    8 => (T0 T1 T2 T3 T4 T5 T6 T7)
+    9 => (T0 T1 T2 T3 T4 T5 T6 T7 T8)
+    10 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9)
+    11 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10)
+    12 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11)
+    13 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12)
+    14 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13)
+    15 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14)
+    16 => (T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14 T15)
 }
 
 #[cfg(test)]
@@ -306,5 +347,12 @@ mod tests {
         let mut buf = Vec::new();
         Vec::<bool>::serialize_header(&mut buf).unwrap();
         assert_eq!(buf, [18, 2]);
+    }
+
+    #[test]
+    fn serialize_tuple() {
+        let mut buf = Vec::new();
+        <((), Option<()>, bool, u8)>::serialize_header(&mut buf).unwrap();
+        assert_eq!(buf, [19, 4, 0, 1, 0, 2, 3]);
     }
 }
