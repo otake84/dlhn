a1f1b272f1f5ad12860b371296955f15ac5ec9ec
Shogo Otake <shogo.otake@gmail.com>
2021-07-29 16:22:27 +0900

    Delete dullahan

--- dullahan/src/message.rs
+++
@@ -1,7 +0,0 @@
-use crate::{body::Body, header::Header};
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct Message {
-    header: Header,
-    body: Body,
-}
--- dullahan/src/stream/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod stream_deserializer;
-pub mod stream_serializer;
