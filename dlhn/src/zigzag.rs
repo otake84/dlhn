9def8006ed24d138aac622262ad4ce98f3e3f13a
Shogo Otake <shogo.otake@gmail.com>
2022-03-11 01:11:31 +0900

    Delete impl ZigZag<u64> for isize

--- dlhn/src/zigzag.rs
+++ dlhn/src/zigzag.rs
@@ -6,25 +6,15 @@ pub(crate) trait ZigZag<T> {
     fn decode_zigzag(value: T) -> Self;
 }
 
-impl ZigZag<u64> for isize {
-    fn encode_zigzag(self) -> u64 {
-        ((self << 1) ^ (self >> 63)) as u64
-    }
+// impl ZigZag<u128> for i128 {
+//     fn encode_zigzag(self) -> u128 {
+//         ((self << 1) ^ (self >> 127)) as u128
+//     }
 
-    fn decode_zigzag(value: u64) -> Self {
-        (value >> 1) as isize ^ (-(value as isize & 1))
-    }
-}
-
-impl ZigZag<u128> for i128 {
-    fn encode_zigzag(self) -> u128 {
-        ((self << 1) ^ (self >> 127)) as u128
-    }
-
-    fn decode_zigzag(value: u128) -> Self {
-        (value >> 1) as i128 ^ (-(value as i128 & 1))
-    }
-}
+//     fn decode_zigzag(value: u128) -> Self {
+//         (value >> 1) as i128 ^ (-(value as i128 & 1))
+//     }
+// }
 
 impl ZigZag<u64> for i64 {
     fn encode_zigzag(self) -> u64 {
